21558f4ac5c070cb1a645f3e5537c9b86eaddd57
BaselAlaa22 <102911455+BaselAlaa22@users.noreply.github.com>
2025-10-09 22:55:11 +0300

    did the navbar and it's css, installed react router and set up some basic routes for now

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,19 @@
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+import Home from "./components/Home.jsx";
+import About from "./components/About.jsx";
+import Navbar from "./components/NavBar.jsx";
+import './index.css'
+
 function App() {
-  return <h1>Hello World</h1>;
+  return (
+    <BrowserRouter>
+      <Navbar/>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,19 @@
+import {Link} from 'react-router-dom';
+
+export default function Navbar() {
+  return (
+    <nav>
+      <ul className='navbar-flex'>
+        <li>
+          <Link className='first-child' to='/'>#VanLife</Link>
+        </li>
+        <li>
+          <Link to='/about'>About</Link>
+        </li>
+        <li>
+          <Link to='/'>Van</Link>
+        </li>
+      </ul>
+    </nav>
+  );
+}
