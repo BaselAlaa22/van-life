18da88715b0b279b94089dc51df15a3fe0069245
BaselAlaa22 <102911455+BaselAlaa22@users.noreply.github.com>
2025-10-14 21:49:07 +0300

    did the about page and it's css

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,3 +1,28 @@
-export default function About(){
-    return <h1>About</h1>
+export default function About() {
+  return (
+    <div className="about">
+      <section className="about-header">
+        <img src="./about.png"  alt="About Us" />
+      </section>
+      <section className="about-body">
+        <h1 className="about-title">
+        Don't squeeze in a sedan when you could relax in a van.
+      </h1>
+      <p className="about-description">
+        Our mission is to enliven your road trip with the perfect travel van
+        rental. Our vans are recertified before each trip to ensure your travel
+        plans can go off without a hitch. (Hitch costs extra 😉) Our team is
+        full of vanlife enthusiasts who know firsthand the magic of touring the
+        world on 4 wheels.
+      </p>
+      <div className="about-explore">
+        <h2 className="about-explore-title">
+          Your destination is waiting.
+          <br /> Your van is ready.
+        </h2>
+        <button className="about-explore-btn">Explore our vans</button>
+      </div>
+      </section>
+    </div>
+  );
 }
