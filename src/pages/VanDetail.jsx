235caae9aa7acd0f3ad340710996ab214831be38
BaselAlaa22 <102911455+BaselAlaa22@users.noreply.github.com>
2025-10-23 01:30:55 +0300

    added back button to van detail page

--- src/pages/VanDetail.jsx
+++ src/pages/VanDetail.jsx
@@ -1,5 +1,6 @@
 import React from "react";
-import { useParams } from "react-router-dom";
+import { useParams, Link } from "react-router-dom";
+import { FaArrowLeftLong } from "react-icons/fa6";
 
 export default function VanDetail() {
   const params = useParams();
@@ -14,17 +15,23 @@ export default function VanDetail() {
 
   return van ? (
     <div className="van-detail">
+      <Link className="clear-btn" to="/vans">
+        <FaArrowLeftLong />
+        Back to vans
+      </Link>
+
       <img className="van-detail-img" src={van.imageUrl} alt={van.name} />
       <div className={`van-type ${van.type}`}>{van.type}</div>
       <h1>{van.name}</h1>
       <h2>
-        $ {van.price} <span style={{fontWeight:500}}>/day</span>
+        $ {van.price} <span style={{ fontWeight: 500 }}>/day</span>
       </h2>
       <p className="van-description">{van.description}</p>
-      <button className="home-link-btn hover-effect click-effect">Rent this van</button>
+      <button className="home-link-btn hover-effect click-effect">
+        Rent this van
+      </button>
     </div>
   ) : (
     <div className="spinner"></div>
   );
 }
-
